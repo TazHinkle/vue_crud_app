3fb517caf85cb3a39c7b5dca3bbbed53119d32d8
Taz Hinkle <hinkle.taz@gmail.com>
2021-07-05 15:36:21 -0600

    Add: Delete on dblclick.

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1>Latest Posts</h1>
     <div class="create-post">
-      <label for="create-post">Say Something</label>
+      <label for="create-post">Add a Phrase: </label>
       <input type="text" id="create-post" v-model="text" placeholder="create a post">
       <button v-on:click="createPost">Post</button>
     </div>
@@ -15,6 +15,7 @@
           v-bind:item="post"
           v-bind:index="index"
           v-bind:key="post._id"
+          v-on:dblclick="deletePost(post._id)"
       >
         {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}` }}
         <p class="text">{{ post.text }}</p>
@@ -45,7 +46,11 @@ export default {
     async createPost() {
       await PostService.insertPost(this.text);
       this.posts = await PostService.getPosts();
-    }
+    },
+    async deletePost(id) {
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
+    },
   }
 };
 </script>
@@ -70,14 +75,6 @@ div.post {
   background-color: #bcffb8;
   padding: 10px 10px 30px 10px;
   margin-bottom: 15px;
-}
-
-div.created-at {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 5px 15px 5px 15px;
-  background-color: darkgreen;
 }
 
 p.text {
